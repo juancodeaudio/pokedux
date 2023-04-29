7efeb33baa23b3dcf4e7b01782cbb783444c7d57
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2023-04-29 18:02:19 -0500

    UI creation and Redux integration wit -legacy createStore-

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { pokemonsReducer } from "./reducers/pokemons.js";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+// import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const store = createStore(pokemonsReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
